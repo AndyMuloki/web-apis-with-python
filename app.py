7ca778149c3b22257915e8f3208b0447aa12c070
Andrew <andrew.muloki@gmail.com>
2025-02-11 20:00:02 +0300

    Implement name greeting logic in the API

--- app.py
+++ app.py
@@ -11,7 +11,19 @@ def index():
     1. Capture first name & last name
     2. If either is not provided: respond with an error
     3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
-    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
-    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
+    4. If first name is provided but second name is not provided: respond with "Hello, <first-name>!"
+    5. If both names are provided: respond with a question, "Is your name <first-name> <second-name>
     """
-    return jsonify("TODO")
+    fname = request.args.get("fname")
+    lname = request.args.get("lname")
+
+    if not fname and not lname:
+        return jsonify({"status" : "error"})
+    elif fname and not lname:
+        return jsonify({"data" : f"Hello, {fname}!"})
+    elif not fname and lname:
+        return jsonify({"data" : f"Hello, Mr. {lname}!"})
+    else:
+        return jsonify({"data" : f"Is your name {fname} {lname}?"})
+    
+    return jsonify(response)
